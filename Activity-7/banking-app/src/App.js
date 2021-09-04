785b405d5643d74444d1b094d0b476bff4afb429
Arwie Mendoza <arwie@avionschool.com>
2021-09-04 19:31:50 +0800

    added files

--- Activity-7/banking-app/src/App.js
+++ Activity-7/banking-app/src/App.js
@@ -1,19 +1,17 @@
 // import React, {useReducer} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation, Footer, Home, Contact, AccountList } from "./components";
+import { Navigation, Footer, Login, Contact, AccountList } from "./components";
 
 function App() {
   return (
     <div>
       <Router>
-        <Navigation />
         <Switch>
-          <Route path="/" exact component={() => <Home />} />
+          <Route path="/" exact component={() => <Login />} />
           <Route path="/accounts" exact component={() => <AccountList />} />
           <Route path="/contact" exact component={() => <Contact />} />
         </Switch>
-        <Footer />
       </Router>
     </div>
   );
