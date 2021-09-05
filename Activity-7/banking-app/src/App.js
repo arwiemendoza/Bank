6290d4c2781929d9e44bfe9c6dbdd9434bad43da
Arwie Mendoza <arwie@avionschool.com>
2021-09-05 10:25:43 +0800

    edited interface

--- Activity-7/banking-app/src/App.js
+++ Activity-7/banking-app/src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div>
       <Router>
-        <Navigation/>
         <Switch>
           <Route path="/Login" exact component={() => <Login />} />
           <Route path="/accounts" exact component={() => <AccountList />} />
