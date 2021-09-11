4a445deedc7d288744e5c6a7bbbd75b35f4a0fb6
blfernandezGIT <blfernandez@up.edu.ph>
2021-09-11 12:24:36 +0800

    api try

--- Activity-7/banking-app/src/components/Client/Dashboard.js
+++ Activity-7/banking-app/src/components/Client/Dashboard.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Dashboard = () => {
+    const [characters, setCharacters] = useState('')
+
+    useEffect(() => {
+        fetch('https://www.breakingbadapi.com/api/characters')
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            setCharacters(data.slice(0,10))
+        })
+    })
+
     return (
         <div>
             Client Dashboard
+            {   characters &&
+                characters.map(character => {
+                return (
+                    <div key={character.char_id}>
+                        <div>{character.name} </div>
+                        <div>{character.nickname} </div>
+                    </div>
+                )
+            })}
+
         </div>
     )
 }
